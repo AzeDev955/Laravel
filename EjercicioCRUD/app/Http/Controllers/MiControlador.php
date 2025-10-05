6fbaa87fd1fa3337e2319e38a2dfb02340942b43
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-05 17:35:24 +0200

    Update MiControlador.php

--- EjercicioCRUD/app/Http/Controllers/MiControlador.php
+++ EjercicioCRUD/app/Http/Controllers/MiControlador.php
@@ -1,18 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Container\Attributes\DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class MiControlador extends Controller
 {
     public function listar(){
-        DB::table("personas")
+        $personas = DB::table('personas')->orderBy('dni', 'asc')->get();
+
+        $datos = [
+            'personas' => $personas
+        ];
+
+        return response()->json($datos,200);
     }
 
     public function listarPersona($dni){
-
+        $persona = DB::table('personas')->where('dni','=', $dni)->get();
+        if($persona){
+            return response()->json($persona,200);
+        }else{
+            $respuesta = 'La persona no existe';
+            return response()->json([$respuesta]) ;
+        }
+        
+        
     }
 
     public function insertar(){
