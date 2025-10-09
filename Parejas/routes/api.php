3ad71455c37352e282cceb702bfa8e015dd6c68a
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-09 13:52:30 +0200

    Update api.php
    inicio rutas

--- Parejas/routes/api.php
+++ Parejas/routes/api.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorPartida;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-
+Route::get('/listar', [MiControlador::class, 'listar']);
+Route::GET('/partida/{id}', [ControladorPartida::class, 'getPartida'])->whereNumber('id');
+Route::get('/partida', [ControladorPartida::class, 'getAllPartidas']);
+Route::post('/partida', [ControladorPartida::class, 'crearPartida']);
+Route::post('/destapar/{num1}/{num2}', [ControladorPartida::class, 'destaparCasilla'])->whereNumber(['num1', 'num2']);
